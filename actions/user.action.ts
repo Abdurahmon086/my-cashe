64aa735ba1a6e7904adb9aa81aa87fc78131a9c6
Abdurahmon086 <1937ali047@gmail.com>
2025-06-30 15:32:07 +0500

    Update dependencies and enhance styling with new CSS variables and animations. Integrate theme provider and toast notifications in layout. Refactor home page to include mode toggle component.

---
+++ actions/user.action.ts
@@ -0,0 +1,94 @@
+"use server";
+
+import { signJwt } from "@/lib/jwt";
+import { connectToDatabase } from "@/lib/mongodb";
+import User from "@/models/User";
+import { IUserDB } from "@/types";
+import bcrypt from "bcryptjs";
+import { cookies } from "next/headers";
+
+export async function createUser(data: IUserDB) {
+  try {
+    await connectToDatabase();
+
+    const existing = await User.findOne({ email: data.email });
+    if (existing) {
+      return { status: 400, data: null, message: "Email already exists" };
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await User.create({
+      ...data,
+      password: hashedPassword,
+    });
+
+    const token = signJwt({ userId: user._id, email: user.email });
+
+    (await cookies()).set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return { status: 200, data: JSON.parse(JSON.stringify({ user, token })), message: "User created successfully" };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { status: 500, data: null, message: "Error creating user" };
+  }
+}
+
+export async function loginUser(email: string, password: string) {
+  try {
+    console.log("asd");
+    await connectToDatabase();
+    const user = await User.findOne({ email });
+    if (!user) {
+      return { status: 401, data: null, message: "Invalid credentials" };
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return { status: 401, data: null, message: "Invalid credentials" };
+    }
+
+    const token = signJwt({ userId: user._id, email: user.email });
+
+    (await cookies()).set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return { status: 200, data: JSON.parse(JSON.stringify({ user, token })), message: "Login successful" };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { status: 500, data: null, message: "Error logging in" };
+  }
+}
+
+export async function getUsers() {
+  try {
+    await connectToDatabase();
+    const users = await User.find();
+    return { status: 200, data: JSON.parse(JSON.stringify(users)), message: "Users fetched successfully" };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { status: 500, data: null, message: "Error getting users" };
+  }
+}
+
+export async function getUser(id: string) {
+  try {
+    await connectToDatabase();
+    const user = await User.findById(id);
+    return { status: 200, data: JSON.parse(JSON.stringify(user)), message: "User fetched successfully" };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { status: 500, data: null, message: "Error getting user" };
+  }
+}
---
+++ components/forms/login-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { loginSchema } from "@/schemas/user.schemas";
+import { loginUser } from "@/actions/user.action";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export function LoginForm({ className, ...props }: React.ComponentProps<"div">) {
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+    try {
+      setIsPending(true);
+      console.log("asd");
+
+      const { status, message } = await loginUser(values.email, values.password);
+      if (status === 200) {
+        toast(message);
+        form.reset();
+        router.push("/");
+      } else {
+        if (message === "Email already exists") {
+          form.setError("email", { message });
+        } else {
+          toast(message);
+        }
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      toast("Error registering user");
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">Login to your account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" {...form.register("email")} />
+                {form.formState.errors.email && <span className="text-sm text-red-500">{form.formState.errors.email.message}</span>}
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" {...form.register("password")} />
+                {form.formState.errors.password && <span className="text-sm text-red-500">{form.formState.errors.password.message}</span>}
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" disabled={isPending}>
+                  {isPending ? "Yuklanmoqda..." : "Login"}
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link href="/auth/register" className="underline underline-offset-4">
+                Sign up
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
